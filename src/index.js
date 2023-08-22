ba8e7a0d41480ae654ff1acd9f666e615a1ce973
Daniel Santa Mateus <dsm50561@hotmail.com>
2023-08-21 22:43:55 -0500

    API cats connected

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import axios from "axios";
 
 axios.defaults.headers.common["x-api-key"] = "live_ZIN1R9wqp2s7M7uoNpVMfKr0CiYm1hplmPwm2fE57xvFcl6kfp2NLh697oDXoEIE";
+
+axios.get('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+        return response;
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error)
+    });
