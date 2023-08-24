c17626aa6458124dfd50e5319c879354f490a8c2
Daniel Santa Mateus <dsm50561@hotmail.com>
2023-08-23 23:08:10 -0500

    breeds images

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.headers.common["x-api-key"] = "live_ZIN1R9wqp2s7M7uoNpVMfKr0CiYm1hplmPwm2fE57xvFcl6kfp2NLh697oDXoEIE";
+axios.defaults.headers.common['x-api-key'] = 'live_ZIN1R9wqp2s7M7uoNpVMfKr0CiYm1hplmPwm2fE57xvFcl6kfp2NLh697oDXoEIE';
 
 export function fetchBreeds() {
     return axios.get('https://api.thecatapi.com/v1/breeds')
@@ -11,3 +11,14 @@ export function fetchBreeds() {
             return response.data;
         })
 }
+
+export function fetchCatByBreed(breedId) {
+    return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+        .then((response) => {
+            if (!response) {
+                throw new Error(error.message);
+            }
+            return response.data;
+        })     
+}
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
-import { fetchBreeds } from "./cat-api";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+
+const breedSelect = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
 
 fetchBreeds().then(data => {
-    const breedSelect = document.querySelector('.breed-select');
     const selectOptions = data
             .map((data) =>`<option value="${data.id}">${data.name}</option>`)
             .join("");
-        breedSelect.innerHTML = selectOptions
+    breedSelect.innerHTML = selectOptions;
     })  
     .catch(error => console.log(error));
-    
+
+breedSelect.addEventListener("change", (e) => {
+    const breedId = e.currentTarget.value;
+    fetchCatByBreed(breedId).then(data => {
+        const breedImg = data
+            .map(data => `<img src="${data.url}"/>`).join("");
+        catInfo.innerHTML = breedInfo;
+        console.log(breedImg);
+        /*
+        const [obj] = data;
+        const breedsArray = obj.breeds;
+        breedsArray.map(el =>
+            `<h3>${el.name}</h3>`).join("");
+        catInfo.innerHTML = breedsArray;
+        */
+    })
+})
+
+
+
+
+
+
+
+
 //const breedSelect = document.querySelector('.breed-select');
 //const loaderMessage = document.querySelector('.loader');
 //const errorMessage = document.querySelector('.error');
