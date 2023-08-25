6b31801cf5a3368d00f5b4bcb3fe400c9bf41261
Daniel Santa Mateus <dsm50561@hotmail.com>
2023-08-24 22:18:35 -0500

    cats information

--- src/index.js
+++ src/index.js
@@ -2,31 +2,41 @@ import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 
 const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
+const loaderMessage = document.querySelector('.loader');
+
+loaderMessage.style.display = "block";
 
 fetchBreeds().then(data => {
     const selectOptions = data
-            .map((data) =>`<option value="${data.id}">${data.name}</option>`)
-            .join("");
+        .map((data) => `<option value="${data.id}">${data.name}</option>`)
+        .join("");
     breedSelect.innerHTML = selectOptions;
-    })  
-    .catch(error => console.log(error));
+})
+    .catch(error => console.log(error))
 
 breedSelect.addEventListener("change", (e) => {
     const breedId = e.currentTarget.value;
     fetchCatByBreed(breedId).then(data => {
-        const breedImg = data
-            .map(data => `<img src="${data.url}"/>`).join("");
-        catInfo.innerHTML = breedInfo;
-        console.log(breedImg);
-        /*
         const [obj] = data;
-        const breedsArray = obj.breeds;
-        breedsArray.map(el =>
-            `<h3>${el.name}</h3>`).join("");
-        catInfo.innerHTML = breedsArray;
-        */
+        const [objBreeds] = obj.breeds;
+        const objInfo = {
+            imgUrl: obj.url,
+            name: objBreeds.name,
+            description: objBreeds.description,
+            temperament: objBreeds.temperament,
+        };
+        const textInfo =
+            `<img src="${objInfo.imgUrl}"/>
+            <h2>${objInfo.name}</h2>
+            <p>${objInfo.description}</p>
+            <p><h3>Temperament:</h3> ${objInfo.temperament}</p>`;
+        catInfo.innerHTML = textInfo;
     })
-})
+        .catch(error => console.log(error))
+        .finally(() => {
+        loaderMessage.style.display = "none";
+    });
+    })
 
 
 
