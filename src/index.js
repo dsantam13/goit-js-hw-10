d07a5a0cdbf324f6df43f2498e7f1b7ebc8c48a1
Daniel Santa Mateus <dsm50561@hotmail.com>
2023-08-28 16:07:44 -0500

    task finished

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 
+const body = document.querySelector('body');
 const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loaderMessage = document.querySelector('.loader');
 const errorMessage = document.querySelector('.error');
 
+body.style.boxSizing = "border-box";
+body.style.fontFamily = "'Roboto', sans-serif";
+body.style.margin = "25px";
 loaderMessage.style.display = "block";
+loaderMessage.style.width = "100%";
+loaderMessage.style.fontWeight = "700";
+loaderMessage.style.marginLeft = "auto";
+loaderMessage.style.marginRigth = "auto";
+
 breedSelect.style.display = "none";
+breedSelect.style.width = "700px";
+
 errorMessage.style.display = "none";
 errorMessage.style.color = "red";
 
@@ -15,17 +26,23 @@ fetchBreeds().then(data => {
         .map((data) => `<option value="${data.id}">${data.name}</option>`)
         .join("");
     breedSelect.innerHTML = selectOptions;
-})
+    })
     .catch((error) => {
         console.log(error.message);
         errorMessage.style.display = "block"
     })
     .finally(() => {
-        breedSelect.style.display = "block";
+        breedSelect.style.display = "flex";   
+        breedSelect.style.boxShadow = "red";
         loaderMessage.style.display = "none";
-});
+        new SlimSelect({
+            select: '.breed-select',
+        })
+        document.documentElement.style.setProperty('--ss-primary-color', '#e59866');
+    });
 
 breedSelect.addEventListener("change", (e) => {
+    e.preventDefault();
     catInfo.style.display = "none";
     loaderMessage.style.display = "block";
     const breedId = e.currentTarget.value;
@@ -40,9 +57,9 @@ breedSelect.addEventListener("change", (e) => {
         };
         const textInfo =
             `<img src="${objInfo.imgUrl}"/>
-            <h2>${objInfo.name}</h2>
+            <div class="text-info"><h2>${objInfo.name}</h2>
             <p>${objInfo.description}</p>
-            <p><h3>Temperament:</h3> ${objInfo.temperament}</p>`;
+            <p><h3>Temperament:</h3> ${objInfo.temperament}</p></div>`;
         catInfo.innerHTML = textInfo;
     })
         .catch((error) => {
@@ -50,7 +67,14 @@ breedSelect.addEventListener("change", (e) => {
         errorMessage.style.display = "block"
     })
         .finally(() => {
-            catInfo.style.display = "block";
             loaderMessage.style.display = "none";
+            catInfo.style.display = "flex";
+            catInfo.style.marginTop = "25px";
+            const imgBreed = document.querySelector("img");
+            const textInfo = document.querySelector("text-info");
+            imgBreed.style.width = "50%";
+            imgBreed.style.height = "auto";
+            imgBreed.style.marginRight = "50px";
     });
 })
+    
