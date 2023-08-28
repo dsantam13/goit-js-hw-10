0c7f8b6abcda42765b2bfc769ea67578becd2188
Daniel Santa Mateus <dsm50561@hotmail.com>
2023-08-27 22:21:50 -0500

    loading message and error message

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loaderMessage = document.querySelector('.loader');
+const errorMessage = document.querySelector('.error');
 
 loaderMessage.style.display = "block";
+breedSelect.style.display = "none";
+errorMessage.style.display = "none";
+errorMessage.style.color = "red";
 
 fetchBreeds().then(data => {
     const selectOptions = data
@@ -12,9 +16,18 @@ fetchBreeds().then(data => {
         .join("");
     breedSelect.innerHTML = selectOptions;
 })
-    .catch(error => console.log(error))
+    .catch((error) => {
+        console.log(error.message);
+        errorMessage.style.display = "block"
+    })
+    .finally(() => {
+        breedSelect.style.display = "block";
+        loaderMessage.style.display = "none";
+});
 
 breedSelect.addEventListener("change", (e) => {
+    catInfo.style.display = "none";
+    loaderMessage.style.display = "block";
     const breedId = e.currentTarget.value;
     fetchCatByBreed(breedId).then(data => {
         const [obj] = data;
@@ -32,20 +45,12 @@ breedSelect.addEventListener("change", (e) => {
             <p><h3>Temperament:</h3> ${objInfo.temperament}</p>`;
         catInfo.innerHTML = textInfo;
     })
-        .catch(error => console.log(error))
+        .catch((error) => {
+        console.log(error.message);
+        errorMessage.style.display = "block"
+    })
         .finally(() => {
-        loaderMessage.style.display = "none";
+            catInfo.style.display = "block";
+            loaderMessage.style.display = "none";
     });
     })
-
-
-
-
-
-
-
-
-//const breedSelect = document.querySelector('.breed-select');
-//const loaderMessage = document.querySelector('.loader');
-//const errorMessage = document.querySelector('.error');
-//const catInfo = document.querySelector('.cat-info');
